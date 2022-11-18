da07c7fbaa41d1e5af95d07329b5174060a08c6e
Keenen Harral <kharral23@yahoo.com>
2022-11-17 19:27:55 -0800

    commit tree afde10817198c7532f01413879402dc079b1c603

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+
+function findRecord(element){
+    return element.result === 'W';
+}
+
+function superbowlWin(array) {
+   const records = array.find(findRecord)
+   if(records) {
+    return records.year
+   } else {
+    return undefined;
+   }
+
+}
+
